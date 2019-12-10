8d7f6f01a2390f25fd5be5b8bd8f382d6c92fd56
Scott Miller <scottmillerjr@gmail.com>
2019-12-10 10:22:41 -0500

    add main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+variable "project" {
+    type = string
+}
+
+provider "google" {
+  credentials = "${file("gcp.json")}"
+  project     = "${var.project}"
+  region      = "us-central1"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "terraform-test-cluster"
+  location           = "us-central1-a"
+  initial_node_count = 3
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    labels = {
+      foo = "bar"
+    }
+
+    tags = ["foo", "bar"]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
